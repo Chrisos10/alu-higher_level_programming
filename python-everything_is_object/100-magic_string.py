bbc4d5300a08bc29da74b17f9d0400e6c40bed16
Chrisos10 <j.dufitumuk@alustudent.com>
2023-07-26 12:26:35 -0700

    returns a string n times updated

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    
-    return "".join("BestSchool" + str(i) for i in range(1, n + 1))
+    setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
+    return ("BestSchool, " * getattr(magic_string, "n", 0))[:-2]
