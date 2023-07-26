55c6fcb8eb7ed22f2a4f5f55d3a79a9cba80971b
Chrisos10 <j.dufitumuk@alustudent.com>
2023-07-26 08:43:40 -0700

    defines a square and sets property and property setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 
-"""Defines a square by a private instance attribute 'size'"""
+"""Define a class Square."""
 
 
 class Square:
-    """defining a square"""
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """
-        defines a square
-        Attributes:
-            size(int): The size of the square
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """Getter method to retrieve"""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
-    def size(swlf, value):
-         """Setter method to initalize value"""
-        if type(value) != int:
-            raise TypeError('size must be an integer')
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        """Returns the area of the square"""
+        """Returns the area of the square."""
         return (self.__size**2)
