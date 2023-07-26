229bd4f7ba405aafd76dbfc8c1debaf5de8b2449
Chrisos10 <j.dufitumuk@alustudent.com>
2023-07-26 08:14:09 -0700

    Returns the area of the current square

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square by a private instance attribute 'size'"""
+
+
+class Square:
+    """defining a square"""
+    def __init__(self, size=0):
+        """
+        defines a square
+        Attributes:
+            size(int): The size of the square
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size**2)
