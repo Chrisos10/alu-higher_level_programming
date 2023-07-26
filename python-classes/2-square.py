c9577a3770a79a3d65083f444dfa79fd7d70e808
Chrisos10 <j.dufitumuk@alustudent.com>
2023-07-26 08:03:13 -0700

    Defines a square with optional instantiation

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a square by a private instance attribute 'size'"""
+
+
+class Square:
+    """defining a square"""
+    def __init__(self, size):
+        """
+        defines a square
+        Attributes:
+            size(int): The size of the square
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
