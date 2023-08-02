56957ac5e6f77a0bdb07d5c6370dd6b3875bc338
Chrisos10 <j.dufitumuk@alustudent.com>
2023-08-02 10:59:44 -0700

    Creating MySQL server updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
--- create a user on server and assign all privileges
--- create user
+-- Create a user user_0d_1 with the password user_0d_1_pwd
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
--- assign all privileges to everything
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
