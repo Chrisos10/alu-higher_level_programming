7163bed4b5b2cd61f499875f709a9ee70a36a809
Chrisos10 <j.dufitumuk@alustudent.com>
2023-08-03 01:49:13 -0700

    0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- a script that creates the MYSQL server user user_od_1
 -- user_od_1 should have all privileges on MYSQL server
 -- THe user_od_1 shuld have password user_od_1_pwd
--- if user_od_1nexists, the script should not fail
-CREATE USER IF NOT ÈXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- if user_od_1 exists, the script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
