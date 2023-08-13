7c0b0784f0c26961a093456264fcc3136e1189c6
Chrisos10 <j.dufitumuk@alustudent.com>
2023-08-13 02:27:05 -0700

    Addition of two integers updated

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,4 +12,4 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if isinstance(b, (int, float)) is False:
         raise TypeError('b must be an integer')
-    return int(a + b)
+    return (int(a) + int(b))
