9004802af6488812dd0a2e669ce8a041b59a73f4
Chrisos10 <j.dufitumuk@alustudent.com>
2023-08-13 06:20:40 -0700

    printing my name updated again!

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-
-"""Print the last and first name and run a bunch of tests to fool the checker"""
-
-
+"""
+Print string
+"""
 def say_my_name(first_name, last_name=""):
-    """Function to return formated name
-    Args:
-        first_name (str) -> The first name
-        last_name (str)  -> The last name
-    Returns:
-        'My name is <first_name> <last_name>'
-    """
-    try:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        else:
-            return ("My name is {} {}".format(first_name, last_name))
-    except Exception as ex:
-        return (ex)
+   """Concatenate both parameters and print
+   Parameters:
+   first_name = first name
+   last_name = last name
+
+   Local Variables:
+   fullname = empty string
+
+   Return: None
+   """
+    fullname = ""
+    if first_name and type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    elif first_name and type(first_name) == str:
+        fullname += first_name + " "
+    if last_name and type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    elif last_name and type(last_name) == str:
+        fullname += last_name
+    print("My name is {:s}".format(fullname))
