d811fc10cb260a810ed063a63d8a2831ad6c8377
Chrisos10 <j.dufitumuk@alustudent.com>
2023-08-17 03:57:31 -0700

    Printing a square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A function that prints a square with the character #
+Run tests for invalid inputs and raise error
+"""
+
 def print_square(size):
     """ Function to print squares of '#'
     Args:
@@ -8,16 +13,25 @@ def print_square(size):
     in which case the exception raised is returned
     """
     try:
-        if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
+        elif not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        elif isinstance(size, float) and size < 0:
-            raise ValueError("size must be an integer")
+        char = ""
+        for i in range(size):
+            for _ in range(size):
+                char += '#'
+            if i != size - 1:
+                char += '\n'
+        if size == 0:
+            return print(end="")
         else:
-            for row in range(size):
-                for column in range(size):
-                    print("#", end="")
-                print()
-    except Exception as e:
-        return (e)
+            return print(char)
+    except TypeError:
+        raise
+    except ValueError:
+        raise
+    except OverflowError:
+        raise
