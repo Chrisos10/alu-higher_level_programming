2feef4e3951538aec0a631328ff8bc7bebeae63b
Chrisos10 <j.dufitumuk@alustudent.com>
2023-09-29 17:53:36 -0700

    print square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (!parseInt(args[2])) {
+  console.log('Missing size');
+} else {
+  for (let timesy = parseInt(args[2]); timesy > 0; timesy--) {
+    let numX = '';
+    for (let timesx = parseInt(args[2]); timesx > 0; timesx--) {
+      numX += 'X';
+    }
+    console.log(numX);
+  }
+}
