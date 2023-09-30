b19a149469ccf9edaf75dfb922f9c11d2005d49a
Chrisos10 <j.dufitumuk@alustudent.com>
2023-09-30 03:48:47 -0700

    created an instance metthod

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let height = 0; height < this.height; height++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
