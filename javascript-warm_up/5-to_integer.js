12c95f60ef338463788dba933ed2d54c36fd9195
Chrisos10 <j.dufitumuk@alustudent.com>
2023-09-29 17:37:54 -0700

    arg converted to int

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-/*
-  print My number: <first argument converted in integer>
-*/
 const args = process.argv;
 if (parseInt(args[2])) {
   console.log(`My number: ${parseInt(args[2])}`);
