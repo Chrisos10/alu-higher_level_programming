59f5f59ec6d611484c96955d1da95bb0f8b20ad4
Chrisos10 <j.dufitumuk@alustudent.com>
2023-10-04 04:20:35 -0700

    Writting a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const nameFile = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(nameFile, str, 'utf8', (err) => {
+  if (err) {
+    return console.error(err);
+  }
+});
